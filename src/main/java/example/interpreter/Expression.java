ca61a7509b73b2ca56eeec32fb709dac3a3e5536
twinky618 <twinky618@gmail.com>
2024-04-27 10:34:43 +0300

    interpreter pattern

---
+++ src/main/java/example/interpreter/Expression.java
@@ -0,0 +1,5 @@
+package example.interpreter;
+
+public interface Expression {
+    boolean interpret(String context);
+}
---
+++ src/main/java/example/interpreter/InterpreterRunner.java
@@ -0,0 +1,26 @@
+package example.interpreter;
+
+public class InterpreterRunner {
+
+    public static void main(String[] args) {
+        var isJava = getJavaExpression();
+        var isJavaDeveloper = getJavaEEExpression();
+
+        System.out.println("Does developer knows Java Core: " + isJava.interpret("Java Core"));
+        System.out.println("Does developer knows Java EE: " + isJavaDeveloper.interpret("Java Spring"));
+    }
+
+    public static Expression getJavaExpression() {
+        var java = new TerminalExpression("Java");
+        var javaCore = new TerminalExpression("Java Core");
+
+        return new OrExpression(java, javaCore);
+    }
+
+    public static Expression getJavaEEExpression() {
+        var java = new TerminalExpression("Java");
+        var spring = new TerminalExpression("Spring");
+
+        return new AndExpression(java, spring);
+    }
+}
