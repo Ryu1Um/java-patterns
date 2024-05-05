b740c1abc8a230d396b180de1fe081e97b152f87
twinky618 <twinky618@gmail.com>
2024-05-05 14:00:43 +0300

    facade pattern

---
+++ src/main/java/example/structural/facade/Workflow.java
@@ -0,0 +1,14 @@
+package example.structural.facade;
+
+public class Workflow {
+
+    Developer developer = new Developer();
+    Job job = new Job();
+    BugTracker bugTracker = new BugTracker();
+
+    public void solveProblems() {
+        job.doJob();
+        bugTracker.startSprint();
+        developer.doJobBeforeDeadline(bugTracker);
+    }
+}
